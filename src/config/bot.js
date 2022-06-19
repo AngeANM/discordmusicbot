1841ef1314ec159b8473518220d459ca5413c796
Angelito G <103382576+AngeANM@users.noreply.github.com>
2022-06-19 20:57:48 +0800

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -3,4 +3,9 @@ require('dotenv').config();
 module.exports = {
     token: process.env.TOKEN,
     prefix: process.env.PREFIX
+    prefix: process.env.PREFIX2
+    prefix: process.env.PREFIX3
+    prefix: process.env.PREFIX4
+    prefix: process.env.PREFIX5
+    prefix: process.env.PREFIX6
 }
